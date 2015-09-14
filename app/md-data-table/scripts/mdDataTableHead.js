7ef48216de54d934873ad487c5c1c47187b296e4
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2015-09-14 04:11:51 +0000

    Add missing var

--- app/md-data-table/scripts/mdDataTableHead.js
+++ app/md-data-table/scripts/mdDataTableHead.js
@@ -94,7 +94,7 @@ angular.module('md.data.table').directive('mdTableHead', ['$document', '$mdTable
       var ngRepeat = tElement.parent().find('tbody').find('tr').attr('ng-repeat');
       
       if(ngRepeat) {
-        mdTableRepeat = $mdTable.parse(ngRepeat);
+        var mdTableRepeat = $mdTable.parse(ngRepeat);
         var items = mdTableRepeat.items;
         var filter = mdTableRepeat.filter;
         var checkbox = angular.element('<md-checkbox></md-checkbox>');
